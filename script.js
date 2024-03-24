759341b5730435462909b9530ae27e70f6c62dbe
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2024-03-24 16:19:04 +0545

    add unfinished game logic

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 class Tile{
     id;
+    row; // index starting from 0
+    col; // index starting from 0
     symbol = "";
     domNode; //? idk
-    /*
-    constructor(id) {
-        this.id = id;
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
     }
-    */
-    toggleCurrentSymbol() {
-        this.symbol = this.symbol == "X" ? "O" : "X";
-        // similary to toggle boolean logic
+    checkGameOver() {
+        let isHorizontalSame = false, isVerticalSame = false, isDiagonalSame = false;
+        // because value of this changes inside functions (that aren't methods)
+        const currentTile = this;
+        // check horizontal
+        isHorizontalSame = tiles[this.row].every(tile => currentTile.symbol == tile.symbol);
+
+        // check vertical
+        isVerticalSame = tiles.map(row => row[currentTile.col]).every(tile => currentTile.symbol == tile.symbol);
+        // check diagonal
+        if (this.col == this.row) {
+            isDiagonalSame = tiles.map((row, i) => row[i]).every(tile => currentTile.symbol == tile.symbol);
+        }
+        return isHorizontalSame || isVerticalSame || isDiagonalSame;
     }
 }
 
 class Board {
+    // NOTE: must be inside Board class because after entering X, the turn is for 0
+    // TODO: fix symbol toggle & errors when clicking
+    toggleCurrentSymbol() {
+        this.symbol = this.symbol == "X" ? "O" : "X";
+        // similar to toggle boolean logic
+    }
 }
 
 const tiles = [[], [], []];
 document.querySelectorAll("#board tr").forEach((row, rNum) => {
-    row.querySelectorAll("td").forEach((col, cNum) => {
+    row.querySelectorAll("td").forEach((tile, cNum) => {
         // NOTE: could use data attribute w/ "dataset" DOM property for row & col number as well
         
         tiles[rNum][cNum] = new Tile();
-        col.onclick = () => {
+        tile.onclick = () => {
             tiles[rNum][cNum].toggleCurrentSymbol(); 
-            col.innerText = tiles[rNum][cNum].symbol;
+            tile.innerText = tiles[rNum][cNum].symbol;
 
+            if(tiles[rNum][cNum].checkGameOver()) alert("Game over");
             // TODO: check game over condition
         }
     })
 })
-/*
-document.querySelectorAll("#board td").forEach((x, i) => {
-    x.onclick = () => {
-        tiles[i].toggleCurrentSymbol();
-        x.innerText = tiles[i].symbol;
-
-    }
-});
-*/
